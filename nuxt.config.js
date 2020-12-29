224e97e09885128109ff89519d2f1c712abad7f7
Wuzado <23276409+Wuzado@users.noreply.github.com>
2020-12-29 16:36:15 +0100

    feat: remove unneeded stuff from the template

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - reddit-viewer',
-    title: 'reddit-viewer',
+    titleTemplate: 'TGN Reddit Viewer',
+    title: 'TGN Reddit Viewer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
