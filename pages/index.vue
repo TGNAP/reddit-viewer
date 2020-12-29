95e80ecf185f35f036f3255cd41ca09c4b585b5e
Wuzado <23276409+Wuzado@users.noreply.github.com>
2020-12-29 20:48:16 +0100

    feat: prepare the ground for filtering

--- pages/index.vue
+++ pages/index.vue
@@ -48,17 +48,122 @@
 export default {
   async fetch() {
     this.posts = await this.$http.$get(
-      'https://api.pushshift.io/reddit/submission/search/?subreddit=teengamingnights'
+      'https://api.pushshift.io/reddit/submission/search/' +
+        // Restrict to a specific subreddit
+        // String or comma-delimited string (Multiple values allowed)
+        '?subreddit=' +
+        this.parameters.subreddit +
+        // Get specific submissions via their ids
+        // Comma-delimited base36 ids
+        '&ids=' +
+        this.parameters.ids +
+        // Search term. Will search ALL possible fields
+        // String / Quoted String for phrases
+        '&q=' +
+        this.parameters.query +
+        // Searches the title field only
+        // String / Quoted String for phrases
+        '&title=' +
+        this.parameters.title +
+        // Searches the selftext (the content of the textposts) field only
+        // String / Quoted String for phrases
+        '&selftext=' +
+        this.parameters.selftext +
+        // Number of results to return
+        // Integer <= 500
+        '&size=' +
+        this.parameters.size +
+        // Sort results in a specific order
+        // "asc", "desc"
+        '&sort=' +
+        this.parameters.sort +
+        // Sort by a specific attribute
+        // "score", "num_comments", "created_utc"
+        '&sort_type=' +
+        this.parameters.sort_type +
+        // Restrict to a specific author
+        // String or comma-delimited string (Multiple values allowed)
+        '&author=' +
+        this.parameters.author +
+        // Return results after this date
+        // Epoch value or Integer + "s,m,h,d" (i.e. 30d for 30 days)
+        '&after=' +
+        this.parameters.after +
+        // Return results before this date
+        // Epoch value or Integer + "s,m,h,d" (i.e. 30d for 30 days)
+        '&before=' +
+        this.parameters.before +
+        // Restrict results based on score
+        // Integer or > x or < x (i.e. score=>100 or score=<25)
+        '&score=' +
+        this.parameters.score +
+        // Restrict results based on number of comments
+        // Integer or > x or < x (i.e. num_comments=>100)
+        '&num_comments=' +
+        this.parameters.num_comments +
+        // Restrict to nsfw or sfw content
+        // "true" or "false"
+        '&over_18=' +
+        this.parameters.over_18 +
+        // Restrict to video content
+        // "true" or "false"
+        '&is_video=' +
+        this.parameters.is_video +
+        // Return locked or unlocked threads only
+        // "true" or "false"
+        '&locked=' +
+        this.parameters.locked +
+        // Return stickied or unstickied content only
+        // "true" or "false"
+        '&stickied=' +
+        this.parameters.stickied +
+        // Exclude or include spoilers only
+        // "true" or "false"
+        '&spoiler=' +
+        this.parameters.spoiler +
+        // Exclude or include content mode submissions
+        // "true" or "false"
+        '&contest_mode=' +
+        this.parameters.contest_mode +
+        // display metadata about the query
+        // ["true", "false"]
+        '&metadata' +
+        this.parameters.metadata
+      // TODO: q:not, title:not, selftext:not, fields, aggs (https://github.com/pushshift/api#using-the-aggs-parameter), frequencys
     )
   },
   data: () => ({
     show: false,
-    query: '',
     posts: {},
+    parameters: {
+      subreddit: 'teengamingnights',
+      ids: '',
+      query: '',
+      title: '',
+      selftext: '',
+      size: 25,
+      fields: '',
+      sort: 'desc',
+      sort_type: 'created_utc',
+      aggs: '',
+      author: '',
+      after: '',
+      before: '',
+      score: '',
+      num_comments: '',
+      over_18: '',
+      is_video: '',
+      locked: '',
+      stickied: '',
+      spoiler: '',
+      contest_mode: '',
+      frequency: '',
+      metadata: '',
+    },
   }),
   methods: {
     sendMessage() {
-      this.clearQuery()
+      this.$fetch()
     },
     clearQuery() {
       this.message = ''
